199c83260509f4ef316e1924ee27b41744f923ca
Pedro Bielik <pedrobielik@hotmail.com>
2024-08-17 17:44:35 +0100

    Background with particlesJS and Google Fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,4 @@
-<script>
-    import "../app.css";
-</script>
-
-<h1 class="text-3xl font-bold">Welcome to Pedro Bielik Portfolio</h1>
+<h1>Welcome to Pedro Bielik Portfolio</h1>
 <p>
     Visit <a href="https://partypeople.world">partypeople.world</a>
 </p>
-
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
-    }
-</style>
