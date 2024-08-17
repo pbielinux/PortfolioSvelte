45dac2c9f65cbdac3c19bf83e6befb4662408be4
Pedro Bielik <pedrobielik@hotmail.com>
2024-08-17 16:56:54 +0100

    Tailwind Integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+</script>
+
+<h1 class="text-3xl font-bold">Welcome to Pedro Bielik Portfolio</h1>
+<p>
+    Visit <a href="https://partypeople.world">partypeople.world</a>
+</p>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
